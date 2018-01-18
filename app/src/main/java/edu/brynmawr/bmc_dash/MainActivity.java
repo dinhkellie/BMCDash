5cfe9147d16a7244347e0c71d36c3cf2199ba128
Kellie Dinh <dinhkellie@yahoo.com>
2018-01-18 15:18:07 -0500

    spinner

--- app/src/main/java/edu/brynmawr/bmc_dash/MainActivity.java
+++ app/src/main/java/edu/brynmawr/bmc_dash/MainActivity.java
@@ -2,12 +2,38 @@ package edu.brynmawr.bmc_dash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView spinner;
+    Random r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        r = new Random();
+        spinner = (ImageView)findViewById(R.id.spinner);
+        spinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RotateAnimation rotate = new RotateAnimation(0, r.nextInt(360 + 360),
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setFillAfter(true);
+                rotate.setDuration(1000);
+                rotate.setInterpolator(new AccelerateDecelerateInterpolator());
+
+                spinner.startAnimation(rotate);
+            }
+        });
     }
 }
